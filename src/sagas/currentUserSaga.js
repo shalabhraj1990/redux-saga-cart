74bedd929878e3b03eb07b1a2c31d03e07fa3aac
shalabhraj1990 <shalabhrajetc@gmail.com>
2022-11-09 11:25:27 +0530

    currenusersaga api chagnes

--- src/sagas/currentUserSaga.js
+++ src/sagas/currentUserSaga.js
@@ -1,8 +1,12 @@
-import { delay } from "redux-saga/effects";
+import { take, put, call, apply } from "redux-saga/effects";
+import fetch from "isomorphic-fetch";
+import { GET_CURRENT_USER_INFO, setCurrentUser } from "../actions";
 
 export function* currentUserSaga() {
-  while (true) {
-    yield delay(1000);
-    console.log("user saga called");
-  }
+  const { id } = yield take(GET_CURRENT_USER_INFO);
+  console.log(id);
+  const response = yield call(fetch, `http://localhost:8081/user/${id}`);
+  const data = yield apply(response, response.json);
+  console.log(data);
+  yield put(setCurrentUser(data));
 }
